6f629588f14b12eb317a0cfed220bcff6b9c588e
cyxou <acyxou@gmail.com>
2015-10-21 09:27:06 +0300

    Indent code

--- src/qwery.js
+++ src/qwery.js
@@ -1,85 +1,78 @@
-(function (name, context, definition) {
-  if (typeof module != 'undefined' && module.exports) module.exports = definition()
-  else if (typeof define == 'function' && define.amd) define(definition)
-  else context[name] = definition()
-})('qwery', this, function () {
-
-  var classOnly = /^\.([\w\-]+)$/;
-  var nodeType = 'nodeType';
-  var isAncestor = 'compareDocumentPosition' in window.document.documentElement ?
-    function (element, container) {
-      return (container.compareDocumentPosition(element) & 16) == 16
-    } :
-    function (element, container) {
-      container = container == window.document || container == window ? window.document.documentElement : container
-      return container !== element && container.contains(element)
-    }
-
-  function toArray(ar) {
-    return [].slice.call(ar, 0)
+var classOnly = /^\.([\w\-]+)$/;
+var nodeType = 'nodeType';
+var isAncestor = 'compareDocumentPosition' in window.document.documentElement ?
+  function (element, container) {
+    return (container.compareDocumentPosition(element) & 16) == 16
+  } :
+  function (element, container) {
+    container = container == window.document || container == window ? window.document.documentElement : container
+    return container !== element && container.contains(element)
   }
 
-  function isNode(el) {
-    var t
-    return el && typeof el === 'object' && (t = el.nodeType) && (t == 1 || t == 9)
-  }
+function toArray(ar) {
+  return [].slice.call(ar, 0)
+}
 
-  function arrayLike(o) {
-    return (typeof o === 'object' && isFinite(o.length))
-  }
+function isNode(el) {
+  var t
+  return el && typeof el === 'object' && (t = el.nodeType) && (t == 1 || t == 9)
+}
 
-  function flatten(ar) {
-    for (var r = [], i = 0, l = ar.length; i < l; ++i) arrayLike(ar[i]) ? (r = r.concat(ar[i])) : (r[r.length] = ar[i])
-    return r
-  }
+function arrayLike(o) {
+  return (typeof o === 'object' && isFinite(o.length))
+}
 
-  function uniq(ar) {
-    var a = [], i, j
-    label:
-    for (i = 0; i < ar.length; i++) {
-      for (j = 0; j < a.length; j++) {
-        if (a[j] == ar[i]) {
-          continue label
-        }
+function flatten(ar) {
+  for (var r = [], i = 0, l = ar.length; i < l; ++i) arrayLike(ar[i]) ? (r = r.concat(ar[i])) : (r[r.length] = ar[i])
+  return r
+}
+
+function uniq(ar) {
+  var a = [], i, j
+  label:
+  for (i = 0; i < ar.length; i++) {
+    for (j = 0; j < a.length; j++) {
+      if (a[j] == ar[i]) {
+        continue label
       }
-      a[a.length] = ar[i]
     }
-    return a
+    a[a.length] = ar[i]
   }
+  return a
+}
 
 
-  function normalizeRoot(root) {
-    if (!root) return window.document
-    if (typeof root == 'string') return qwery(root)[0]
-    if (!root[nodeType] && arrayLike(root)) return root[0]
-    return root
+function normalizeRoot(root) {
+  if (!root) return window.document
+  if (typeof root == 'string') return qwery(root)[0]
+  if (!root[nodeType] && arrayLike(root)) return root[0]
+  return root
+}
+
+/**
+ * @param {string|Array.<Element>|Element|Node} selector
+ * @param {string|Array.<Element>|Element|Node=} opt_root
+ * @return {Array.<Element>}
+ */
+function qwery(selector, opt_root) {
+  var m, root = normalizeRoot(opt_root)
+  if (!root || !selector) return []
+  if (selector === window || isNode(selector)) {
+    return !opt_root || (selector !== window && isNode(root) && isAncestor(selector, root)) ? [selector] : []
   }
-
-  /**
-   * @param {string|Array.<Element>|Element|Node} selector
-   * @param {string|Array.<Element>|Element|Node=} opt_root
-   * @return {Array.<Element>}
-   */
-  function qwery(selector, opt_root) {
-    var m, root = normalizeRoot(opt_root)
-    if (!root || !selector) return []
-    if (selector === window || isNode(selector)) {
-      return !opt_root || (selector !== window && isNode(root) && isAncestor(selector, root)) ? [selector] : []
-    }
-    if (selector && arrayLike(selector)) return flatten(selector)
+  if (selector && arrayLike(selector)) return flatten(selector)
 
 
-    if (window.document.getElementsByClassName && selector == 'string' && (m = selector.match(classOnly))) {
-      return toArray((root).getElementsByClassName(m[1]))
-    }
-    // using duck typing for 'a' window or 'a' document (not 'the' window || document)
-    if (selector && (selector.document || (selector.nodeType && selector.nodeType == 9))) {
-      return !opt_root ? [selector] : []
-    }
-    return toArray((root).querySelectorAll(selector))
+  if (window.document.getElementsByClassName && selector == 'string' && (m = selector.match(classOnly))) {
+    return toArray((root).getElementsByClassName(m[1]))
   }
+  // using duck typing for 'a' window or 'a' document (not 'the' window || document)
+  if (selector && (selector.document || (selector.nodeType && selector.nodeType == 9))) {
+    return !opt_root ? [selector] : []
+  }
+  return toArray((root).querySelectorAll(selector))
+}
 
-  qwery.uniq = uniq
+qwery.uniq = uniq
 
-  return qwery
-}, this);
+module.exports = qwery;
